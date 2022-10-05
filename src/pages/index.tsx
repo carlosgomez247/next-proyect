760245b358bedb964cefa710d314e61eaff2717f
carlosgomez247 <carlosgomez.x247@gmail.com>
2022-10-05 18:56:34 -0500

    lets see

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,20 @@
-import { trpc } from "../utils/trpc"
+//import { trpc } from "../utils/trpc"
 
 export default function Home(){
 
-  const {data,isLoading} = trpc.useQuery(["hello"])
-  if(isLoading){
-    return <div>loading</div>
-  }
-  if (data){
-    return <div>{data!.greeting}</div>
-  }
+  // const {data,isLoading} = trpc.useQuery(["hello"])
+  // if(isLoading){
+  //   return <div>loading</div>
+  // }
+  // if (data){
+  //   return <div>{data!.greeting}</div>
+  // }
 
 
   return <div className="h-screen w-screen flex flex-col justify-center items-center">
 
-    <div className="text-2xl- text-center">Which pokemon is roundest</div>
-    <div className="p-2"></div>
-    <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-    <div className="w-16 h-16 bg-red-200"></div>
-    <div className="p-8">Vs</div>
-    <div className="w-16 h-16 bg-red-200"></div>
-
-    </div>
+    <div className="text-2xl- text-center">Tinotas</div>
+    
 
   </div>
 }
